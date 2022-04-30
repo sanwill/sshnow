6ad144bae1eb70ebc4db37c33201ac95e2117836
sanwill <45527038+sanwill@users.noreply.github.com>
2022-04-29 20:03:14 -0500

    Create sshnow.py

---
+++ sshnow.py
@@ -0,0 +1,27 @@
+import paramiko
+import sys
+
+if __name__ == '__main__':
+    args_len = (len(sys.argv))
+    if args_len != 5:
+        print("Missing arguments.\n"
+              + "Commands: python3 sshnow.py <ip/hostname> <username> <password> <commands in quote>\n"
+              + "Example: python3 sshnow.py 1.2.3.4 username password 'hostname\n"
+              + "Exit."
+              )
+        exit
+    elif args_len == 5:
+        target_host = sys.argv[1]
+        user_name = sys.argv[2]
+        user_pwd = sys.argv[3]
+        cmds = sys.argv[4]
+
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=target_host, username=user_name, password=user_pwd)
+
+        stdin, stdout, stderr = ssh_client.exec_command(cmds)
+        cmds_out = stdout.read().decode().strip()
+        print(cmds_out)
+
+        del ssh_client, stdin, stdout, stderr
